ba47b35ecc2a7490fdac524dcbb035852ff6d2f7
Lv-CollapseX <147068198+Lv-CollapseX@users.noreply.github.com>
2024-10-30 14:54:26 +0800

    Add files via upload

---
+++ springboot/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String telephone_number;
+}
